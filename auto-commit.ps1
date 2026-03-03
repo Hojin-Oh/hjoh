c27718ba9e8fca1669abe8fc22d362700c330d6c
오호진 <hjoh@louarchitects.com>
2026-03-03 12:47:34 +0900

    자동 커밋: 2026-03-03 12:47

--- auto-commit.ps1
+++ auto-commit.ps1
@@ -1,10 +1,14 @@
-# Obsidian Vault 자동 커밋 & 푸시 스크립트
+﻿# Obsidian Vault 자동 커밋 & 푸시 스크립트
 # Windows 작업 스케줄러에 등록하여 주기적으로 실행
+
+$OutputEncoding = [System.Text.Encoding]::UTF8
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
 $vaultPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 $logFile   = "$vaultPath\auto-commit.log"
 
 Set-Location $vaultPath
+$env:GIT_TERMINAL_PROMPT = "0"
 
 # 변경 사항 확인
 $status = git status --porcelain
@@ -19,4 +23,4 @@ git commit -m "자동 커밋: $timestamp"
 git push origin master
 
 # 결과 로그 기록
-"[$timestamp] 커밋 완료" | Out-File -Append -FilePath $logFile
+"[$timestamp] 커밋 완료" | Out-File -Append -FilePath $logFile -Encoding UTF8
